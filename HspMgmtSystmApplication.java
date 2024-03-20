8ea30ff2ae9a2bdb2d2eccc8c86449419aa58fe0
Kanchan Santosh Kothar <139263691+kanchangitkothar@users.noreply.github.com>
2024-03-20 11:39:18 +0530

    Add files via upload

---
+++ HspMgmtSystmApplication.java
@@ -0,0 +1,12 @@
+package com.jsp.hsp_mgmt_systm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HspMgmtSystmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HspMgmtSystmApplication.class, args);
+	}
+}
